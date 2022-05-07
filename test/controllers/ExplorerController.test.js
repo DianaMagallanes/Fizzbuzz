100d5d86416f3ee8fbc23aa1900181a8a20519af
DianaMagallanes <dianakarina2003@example.com>
2022-05-06 21:27:50 -0700

    Agregando pruebas Telegrambot a ExplorerController

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -20,6 +20,68 @@ describe("Test for ExplorerController",()=> {
     test("4. Fizzbuzz status",()=>{
         const result = ExplorerController.getFizzbuzzStatus(15);
         expect(result).toBe("FIZZBUZZ");        
+    });    
+
+    test("5. TelegramBot ON",()=>{
+        const result = ExplorerController.getOnTextBot( {
+            "message_id": 40,
+            "id": 1234567890,
+            "is_bot": false,
+            "first_name": "Diana",
+            "username": "Diana0",
+            "language_code": "es",
+            "chat": {
+                "id": 1234567890,
+                "first_name": "Diana",
+                "type": "private"
+            },
+            "date": 1651895384,
+            "text": "15"
+        }, [ "A", "B"] );
+        expect(result[0]).toBe(1234567890);        
+    }); 
+
+  
+    test("6. TelegramBot status number",()=>{
+        const result = ExplorerController.getonValidationBot(
+            {
+                "message_id": 40,
+                "id": 1234567890,
+                "is_bot": false,
+                "first_name": "Diana",
+                "username": "Diana0",
+                "language_code": "es",
+                "chat": {
+                    "id": 1234567890,
+                    "first_name": "Diana",
+                    "type": "private"
+                },
+                "date": 1651895384,
+                "text": "15"
+            }
+        );
+        expect(result[1]).toBe("Tu número es: 15. Validación: FIZZBUZZ");        
+    });
+
+    test("7. TelegramBot status mission",()=>{
+        const result = ExplorerController.getonValidationBot(
+            {
+                "message_id": 40,
+                "id": 1234567890,
+                "is_bot": false,
+                "first_name": "Diana",
+                "username": "Diana0",
+                "language_code": "es",
+                "chat": {
+                    "id": 1700414804,
+                    "first_name": "Diana",
+                    "type": "private"
+                },
+                "date": 1234567890,
+                "text": "java"
+            }
+        );
+        expect(result[1]).toBe("Los siguientes explorers estan en la mission de java: ajolonauta6,ajolonauta7,ajolonauta8,ajolonauta9,ajolonauta10");        
     });
    
 });
